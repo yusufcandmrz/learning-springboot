be3e9df68343fc4df05f95375b9944ec31f2fa16
Yusuf Demiroz 00858166 <yusufcandmrz@gmail.com>
2024-08-26 10:26:10 +0300

    Updated 01-spring-boot-overview and created 02-spring-boot-spring-core

---
+++ 02-spring-boot-spring-core/springcoredemo/src/test/java/com/yusufcandmrz/springboot/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusufcandmrz.springboot.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
