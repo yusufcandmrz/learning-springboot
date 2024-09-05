39529f6dacb981864f03714a85e4b47ee15484fe
Yusuf Demiroz 00858166 <yusufcandmrz@gmail.com>
2024-09-05 16:49:50 +0300

    Updated 04-spring-boot-rest-crud

---
+++ 04-spring-boot-rest-crud/demo/src/main/java/com/yusufcandmrz/springboot/demo/rest/StudentRestController.java
@@ -0,0 +1,33 @@
+package com.yusufcandmrz.springboot.demo.rest;
+
+import com.yusufcandmrz.springboot.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    List<Student> students;
+
+    @PostConstruct()
+    public void loadData() {
+        students = new ArrayList<Student>();
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId) {
+        return students.get(studentId);
+    }
+}
