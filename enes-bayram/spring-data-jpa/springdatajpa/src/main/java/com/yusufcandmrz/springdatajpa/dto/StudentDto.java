4748f83b29ce9a2801d51f2bfa1b05e7e58c4d2b
Yusuf Demiroz <yusufcandmrz@gmail.com>
2025-12-07 16:08:48 +0300

    Add StudentDto, StudentDtoIU dto classes

---
+++ enes-bayram/spring-data-jpa/springdatajpa/src/main/java/com/yusufcandmrz/springdatajpa/dto/StudentDto.java
@@ -0,0 +1,14 @@
+package com.yusufcandmrz.springdatajpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDto {
+    private String fullName;
+}
