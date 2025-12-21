67c2a120ce2bc63ad27fb01e2a203335f0f3120e
Yusuf Demiroz <yusufcandmrz@gmail.com>
2025-12-21 12:11:27 +0300

    Add @NotBland annotation to Dto classes

--- enes-bayram/spring-data-jpa/springdatajpa/src/main/java/com/yusufcandmrz/springdatajpa/dto/DepartmentDto.java
+++ enes-bayram/spring-data-jpa/springdatajpa/src/main/java/com/yusufcandmrz/springdatajpa/dto/DepartmentDto.java
@@ -1,5 +1,6 @@
 package com.yusufcandmrz.springdatajpa.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DepartmentDto {
+
+    @NotBlank
     private String name;
 }
--- enes-bayram/spring-data-jpa/springdatajpa/src/main/java/com/yusufcandmrz/springdatajpa/dto/StudentProfileDto.java
+++ enes-bayram/spring-data-jpa/springdatajpa/src/main/java/com/yusufcandmrz/springdatajpa/dto/StudentProfileDto.java
@@ -1,5 +1,6 @@
 package com.yusufcandmrz.springdatajpa.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class StudentProfileDto {
 
+    @NotBlank
     private String address;
 }
